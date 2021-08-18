0296237cafaba8ec149d2c93a34eefb97530298c
yaniv92648 <yaniv92648@gmail.com>
2021-08-18 11:35:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import io
 st.title('File upload app!')
 
 file = st.sidebar.file_uploader()
-text_io = io.TextIOWrapper(file,encoding='UTF-8')
-st.write(list(text_io.readlines()))
+# text_io = io.TextIOWrapper(file,encoding='UTF-8')
+# st.write(list(text_io.readlines()))
