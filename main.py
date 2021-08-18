4c4b89ea473da4850ecf671be488e02f6ffd4ee8
yaniv92648 <yaniv92648@gmail.com>
2021-08-18 11:50:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.title('File upload app!')
-st.file_uploader('')
+filename = st.file_uploader('').name
+df = pd.read_csv(filename, delimiter = "\t", encoding='utf-8')
+display(df)
