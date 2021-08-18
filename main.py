ed301355cfc37dcb25aead3676d2364bdaf40be3
yaniv92648 <yaniv92648@gmail.com>
2021-08-18 09:41:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title('File upload app!')
 
-file = st.sidebar.file_uploader("Upload File")
+file = st.sidebar.file_uploader(type="txt", encoding = 'utf8')
 st.write(file.readlines())
