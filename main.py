a22d19c71f8139c9dd11dc9236e8a91a42854545
yaniv92648 <yaniv92648@gmail.com>
2021-08-18 11:50:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 st.title('File upload app!')
 filename = st.file_uploader('').name
-df = pd.read_csv(filename, delimiter = "\t", encoding='utf-8')
+df = pd.read_csv('chat.txt', delimiter = "\t", encoding='utf-8')
 display(df)
