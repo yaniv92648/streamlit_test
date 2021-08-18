ef9dab09b8577cba776f2faa9e168da8f4d628c8
yaniv92648 <yaniv92648@gmail.com>
2021-08-18 11:54:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 
 st.title('File upload app!')
-filename = st.file_uploader('').name()
-with open(filename, encoding="utf-8") as file:
-  total_chat = file.readlines()
-st.write(total_chat)
+filename = st.file_uploader('').name
+st.write(f'This is the file name: {filename}')
+# with open(filename, encoding="utf-8") as file:
+#   total_chat = file.readlines()
+# st.write(total_chat)
