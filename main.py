8ff2aaec189e4192024dcaf1b4f238ebce559cf7
yaniv92648 <yaniv92648@gmail.com>
2021-08-18 12:19:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 
 st.title('File upload app!')
-file = st.file_uploader('')
-full_text = file.read().decode("utf-8")
-st.write(full_text)
+
+def get_total_chat():
+  file = st.file_uploader('').readlines()
+  return [line.decode("utf-8") for line in file]
+
+st.write(get_total_chat())
