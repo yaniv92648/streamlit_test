f5429230a47be3a7d7d4f7bcbff93c526a87c4e3
yaniv92648 <yaniv92648@gmail.com>
2021-08-18 11:55:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title('File upload app!')
 filename = st.file_uploader('').name
-st.write(f'This is the file name: {filename}')
+st.write(f'This is the file name:_{filename}_')
 # with open(filename, encoding="utf-8") as file:
 #   total_chat = file.readlines()
 # st.write(total_chat)
